34cd1e4a0d8b1de28965f4fa5c534e028f922d9d
Ol1mowski <olimarki123@wp.pl>
2023-11-03 19:38:07 +0100

    add sketch home section without tech stack

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/header";
+import Home from "./components/home/home";
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
