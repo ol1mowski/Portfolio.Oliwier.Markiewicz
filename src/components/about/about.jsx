c3369e828a675954f32fdfacebd0c66a8649ecb0
Ol1mowski <olimarki123@wp.pl>
2023-11-04 14:08:38 +0100

    finish introducing about section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,13 +1,16 @@
 import style from './about.module.scss';
 import about from '../../assets/images/about.webp';
-
+// import dev_icon from '../../assets/icons/about_icon.svg';
+// import icon from '../../assets/icons/dev.png';
 
 const About = () => {
     return (
         <>
-            <section className={style.aboutContainer}>
+            <section id='about' className={style.aboutContainer}>
                 <div className={style.aboutContainer__imageSection}>
                     <img src={about} alt='work place' className={style.aboutContainer__imageSection__img} />
+                    {/* <img src={dev_icon} alt='web dev icon' className={style.aboutContainer__imageSection__imgDev} />
+                    <img src={icon} alt='dev icon' className={style.aboutContainer__imageSection__iconDev} /> */}
                 </div>
                 <div className={style.aboutContainer__contentSection}>
                     <h3 className={style.aboutContainer__contentSection__h3}>
