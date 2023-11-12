f3c82b9cd835cded1541f1c8a6c595bbcae119ef
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2023-11-12 22:58:45 +0100

    refactoring components header and project

---
+++ src/components/header/MenuItem/MenuItem.jsx
@@ -0,0 +1,28 @@
+// MenuItem.js
+import React from "react";
+import { Link } from "react-scroll";
+import { motion } from "framer-motion";
+import style from './MenuItem.module.scss';
+
+const MenuItem = ({ to, label, index, buttonVariants, closeRef }) => (
+  <Link
+    key={index}
+    activeClass="active"
+    to={to}
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration={1200}
+  >
+    <motion.div
+      ref={closeRef}
+      className={style.item}
+      variants={buttonVariants}
+      whileHover="hover"
+    >
+      {label}
+    </motion.div>
+  </Link>
+);
+
+export default MenuItem;
